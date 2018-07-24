aadf7a98f921208c4ed7519238e73ad5d18f9b3c
lelezhangzhao <lelezhangzhao@163.com>
2018-07-24 10:33:20 +0800

    备份

--- tp5/application/gjcf/model/LoginRecord.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace app\gjcf\model;
-
-use think\Model;
-
-class Loginrecord extends Model
-{
-
-}
--- tp5/application/gjcf/model/Ydcrecord.php
+++ tp5/application/gjcf/model/Ydcrecord.php
@@ -6,7 +6,7 @@ use think\Model;
 class Ydcrecord extends Model{
 
     protected function getYdcrecordtypeAttr($value){
-        $types = [0 => '充值', 1 => '提现', 2 => '正常收益', 3 => '签到', 4 => '一级推荐奖', 5 => '二级推荐奖', 6 => '三级推荐奖', 7 => '分红'];
+        $types = [0 => '充值', 1 => '提现', 2 => '正常收益', 3 => '签到', 4 => '一级推荐奖', 5 => '二级推荐奖', 6 => '三级推荐奖', 7 => '分红', 8 => '注册'];
         return $types[$value];
     }
 
